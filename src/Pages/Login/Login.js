ad5b0cbf1a38b6597ebd58f3faffd0133f19d9ee
Fahmida Muntasim <fahmidamuntasimt@gmail.com>
2022-11-10 11:09:01 +0600

    auth

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useState } from 'react';
+import { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
 
-    const handleLogin = event => {
+    const [ error, setError] = useState('')
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/';
+
+    const navigate = useNavigate()
+
+
+    const { providerLogin, signIn } = useContext(AuthContext);
+    const googleProvider = new GoogleAuthProvider()
+
+    const handleGoogleSignIn = () => {
+        providerLogin(googleProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error))
+    }
+
+    const handleSubmit = event => {
         event.preventDefault();
+        const form = event.target;
+
+        const email = form.email.value;
+        const password = form.password.value;
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+
+                form.reset();
+                setError('')
+                navigate(from, { replace: true })
+
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message)
+            })
+
     }
 
     return (
@@ -15,24 +58,24 @@ const Login = () => {
 
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handleLogin} className="card-body">
+                        <form onSubmit={handleSubmit} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input name="email" type="email" placeholder="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input name="password" type="password" placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-outline bg-red-900">Login</button>
+                                <button className="btn btn-outline bg-red-900" onClick={handleGoogleSignIn}>Login</button>
                             </div>
                         </form>
                     </div>
